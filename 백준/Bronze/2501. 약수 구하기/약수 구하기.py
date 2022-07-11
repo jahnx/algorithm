44357f28b5ddc8c53d69650e6ef1c0afe8ded031
Jae Ahn <jahn739@gmail.com>
2022-07-11 20:50:57 +0900

    Update 약수 구하기.py

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,4 +1,6 @@
 n, k = list(map(int, input().split()))
+
+# 첫번째 풀이
 arr = []
 
 for i in range (1, n + 1):
@@ -9,3 +11,15 @@ if (len(arr) < k):
     print(0)
 else:
     print(arr[k-1])
+    
+# 두번째 풀이
+cnt = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        cnt += 1
+    if cnt == k:
+        print(i)
+        break
+else:
+    print(0)
